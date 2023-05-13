3ffae2c67487a82d7209484f6514e8ee99e045a1
Vladimir Shevchuk <5t4lk.dev@gmail.com>
2023-05-13 18:56:31 +0100

    creating architecture

--- cmd/main.go
+++ cmd/main.go
@@ -3,11 +3,14 @@ package main
 import (
 	"log"
 	"notes"
+	"notes/pkg/handler"
 )
 
 func main() {
-	srv := new(notes.Server)
-	if err := srv.Run("8000"); err != nil {
+	handlers := new(handler.Handler)
+
+	srv := new(todo_app.Server)
+	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err)
 	}
 }
