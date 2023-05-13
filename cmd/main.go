8af4421c2979a274ec1e699f0d4d913617f73f65
Vladimir Shevchuk <5t4lk@Vladimirs-MacBook-Air.local>
2023-05-13 18:14:52 +0100

    edited main

--- cmd/main.go
+++ cmd/main.go
@@ -8,6 +8,6 @@ import (
 func main() {
 	srv := new(notes.Server)
 	if err := srv.Run("8000"); err != nil {
-		log.Fatalf("error occured while running http server: %s", err)
+		log.Fatalf("error occured while running http server:%s", err)
 	}
 }
