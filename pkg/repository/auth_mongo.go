1b1065028ea1ef046772e64471a412e3bb9756c8
Vladimir Shevchuk <5t4lk.dev@gmail.com>
2023-05-20 01:08:53 +0100

    implemented authorization logic

--- pkg/repository/auth_mongo.go
+++ pkg/repository/auth_mongo.go
@@ -2,6 +2,7 @@ package repository
 
 import (
 	"context"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"todo"
@@ -16,6 +17,8 @@ func NewAuthMongo(collection *mongo.Collection) *AuthMongo {
 }
 
 func (r *AuthMongo) CreateUser(user todo.User) (string, error) {
+	user.Id = primitive.NewObjectID().Hex()
+
 	result, err := r.collection.InsertOne(context.TODO(), user)
 	if err != nil {
 		return "", err
@@ -24,4 +27,19 @@ func (r *AuthMongo) CreateUser(user todo.User) (string, error) {
 	id := result.InsertedID.(primitive.ObjectID).Hex()
 
 	return id, nil
+}
+
+func (r *AuthMongo) GetUser(username, password string) (todo.User, error) {
+	var user todo.User
+
+	filter := bson.M{
+		"username": username,
+		"password": password,
+	}
+
+	if err := r.collection.FindOne(context.TODO(), filter).Decode(&user); err != nil {
+		return todo.User{}, err
+	}
+
+	return user, nil
 }
--- pkg/service/auth.go
+++ pkg/service/auth.go
@@ -3,11 +3,17 @@ package service
 import (
 	"crypto/sha1"
 	"fmt"
+	"github.com/dgrijalva/jwt-go"
+	"time"
 	"todo"
 	"todo/pkg/repository"
 )
 
-const salt = "3c45caertave8m9puklgn"
+const (
+	salt       = "3c45caertave8m9puklgn"
+	signingKey = "qrkjk#4#%35FSFJlja#4353KSFjH"
+	tokenTTL   = 12 * time.Hour
+)
 
 type AuthService struct {
 	repo repository.Authorization
@@ -20,6 +26,21 @@ func NewAuthService(repo repository.Authorization) *AuthService {
 func (s *AuthService) CreateUser(user todo.User) (string, error) {
 	user.Password = generatePasswordHash(user.Password)
 	return s.repo.CreateUser(user)
+}
+
+func (s *AuthService) GenerateToken(username, password string) (string, error) {
+	user, err := s.repo.GetUser(username, generatePasswordHash(password))
+	if err != nil {
+		return "", err
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.StandardClaims{
+		Subject:   user.Id,
+		IssuedAt:  time.Now().Unix(),
+		ExpiresAt: time.Now().Add(tokenTTL).Unix(),
+	})
+
+	return token.SignedString([]byte(signingKey))
 }
 
 func generatePasswordHash(password string) string {
